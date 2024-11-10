bc6ca94122cdbc7a9b26322c5e8b9795e7800107
chillo-tech <achille.mbougueng@chillo.tech>
2024-11-10 13:47:13 +0100

    Injection des dépendances

--- src/main/java/tech/chillo/naissances/profiles/ProfilesController.java
+++ src/main/java/tech/chillo/naissances/profiles/ProfilesController.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
     Controlleur pour gérer les opérations
@@ -22,10 +24,20 @@ public class ProfilesController {
 
     private final ProfilesService profilesService;
 
-    @PostMapping
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public void create(@RequestBody Profile profile) {
         log.info("Création du compte {} ", profile.getEmail());
         this.profilesService.create(profile);
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public List<Profile> search() {
+        return this.profilesService.search();
+    }
+
+    @GetMapping(path = "{id}")
+    public Profile read(@PathVariable int id) {
+        return this.profilesService.read(id);
     }
 
 }
--- src/main/java/tech/chillo/naissances/profiles/ProfilesService.java
+++ src/main/java/tech/chillo/naissances/profiles/ProfilesService.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -13,9 +16,17 @@ public class ProfilesService {
     private final ProfilesRepository profilesRepository;
 
     public void create(Profile profile) {
-
         log.info("Nouveau compte avec l'email {}", profile.getEmail() );
         this.profilesRepository.save(profile);
 
+    }
+
+    public List<Profile> search() {
+        return this.profilesRepository.findAll();
+    }
+
+    public Profile read(int id) {
+        Optional<Profile> profileOptional = this.profilesRepository.findById(id);
+        return profileOptional.orElse(null);
     }
 }
