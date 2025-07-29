dfb8e4d01c8e316bdf5a644d3e421f9d02b706c7
Serge-Mane <sergemane81@gmail.com>
2025-07-29 16:23:57 +0000

    deployer le back

--- src/main/resources/db/migration/V1.17__add_users.sql
+++ src/main/resources/db/migration/V1.17__add_users.sql
@@ -1,4 +1,4 @@
 insert into profiles (first_name, last_name, password, email, active, roles_id)
 values
-('Agent', 'sam.tech', '$2a$10$OWWt4EEfhFr6LW/e4mx9n.3SnOSeUsxw3zSvxX/paTQ//h1bfOa4u', 'agent@chillo.tech', true, (select  id from roles where  name = 'AGENT')),
-('Admin', 'sam.tech', '$2a$10$OWWt4EEfhFr6LW/e4mx9n.3SnOSeUsxw3zSvxX/paTQ//h1bfOa4u', 'admin@chillo.tech', true, (select  id from roles where  name = 'ADMINISTRATOR'));
+('Agent', 'sam.tech', '$2a$10$OWWt4EEfhFr6LW/e4mx9n.3SnOSeUsxw3zSvxX/paTQ//h1bfOa4u', 'agent@sam.tech', true, (select  id from roles where  name = 'AGENT')),
+('Admin', 'sam.tech', '$2a$10$OWWt4EEfhFr6LW/e4mx9n.3SnOSeUsxw3zSvxX/paTQ//h1bfOa4u', 'admin@sam.tech', true, (select  id from roles where  name = 'ADMINISTRATOR'));
